44b6e6c3561458d978b01e234235029b683a601e
Jens Vialkowitsch <jens@vialkowitsch.de>
2019-01-19 09:05:24 +0100

    Add files via upload
    changed Zisterne Volume to appropriate value - starting temperature read integration.
    
    Todo: refactor to asynchronous REST service where values are only requested from internal persistence - remove synchronous calls. create config file and do initialization tasks once at startup (login to digitalstrom). rethink REST-Path naming to get a more ubiquious naming convention

--- src/main/java/RESTController/RESTController.java
+++ src/main/java/RESTController/RESTController.java
@@ -41,16 +41,20 @@ class MeasurementController {
 		return repository.save(newMeasurement);
 	}
 
-  @RequestMapping("/api/measurement/current")
-  public Measurement current() {
-    // 5,8 cm Volumen, max Höhe 2600-620=1980mm
-      String Host="192.168.3.207";
-      int Port = 25000;
-      byte number[] = new byte[2];
+  @RequestMapping("/api/temperatures/current")
+  public Temperatures current() {
+      String Host="192.168.3.11";
+      String ApplicationToken="83b9926dcf6d3d4a8f5928b44b0904d0763507fe117e36052fedc0afbc817869";
+      int Port = 8080;
+
+//    https://192.168.3.11:8080/json/system/loginApplication?loginToken=83b9926dcf6d3d4a8f5928b44b0904d0763507fe117e36052fedc0afbc817869
+//    https://192.168.3.11:8080/json/apartment/getSensorValues
+    /*  byte number[] = new byte[2];
       byte rawinput[] = new byte[10];
       byte [] Delivery_Conf = {(byte)0x01, (byte)0x03, (byte)0x00, (byte)0x04,
                                (byte)0x00, (byte)0x01, (byte)0xc5, (byte)0xcb};
       short num=0;
+
       try {
         InetAddress host = InetAddress.getByName(Host);
         clientSocket = new Socket(host, Port);
@@ -67,7 +71,8 @@ class MeasurementController {
         dOut.write(Delivery_Conf);
 
         dIn.read(rawinput);
-        num = (short) (rawinput[3]<<8 | rawinput[4]);
+        num = (short)( (rawinput[3] & 0xFF) << 8 | (rawinput[4]&0xFF));
+        //System.out.println(String.format("%d %s %s",num,String.format("%02X", rawinput[3]),String.format("%02X", rawinput[4])));
 
         dIn.close();
         dOut.close();
@@ -76,7 +81,50 @@ class MeasurementController {
       catch(IOException e){
         e.printStackTrace();
       }
-      Measurement measure = new Measurement ("level",Short.toString(num),num/19.80);
+      Measurement measure = new Measurement ("level",Short.toString(num),num/(size/100));*/
+      return measure;
+  }
+
+  @RequestMapping("/api/measurement/current")
+  public Measurement current() {
+    // 5,8 cm Volumen, max Höhe 2600-620=1980mm
+      String Host="192.168.3.207";
+      int Port = 25000;
+      double size=1742;
+
+      byte number[] = new byte[2];
+      byte rawinput[] = new byte[10];
+      byte [] Delivery_Conf = {(byte)0x01, (byte)0x03, (byte)0x00, (byte)0x04,
+                               (byte)0x00, (byte)0x01, (byte)0xc5, (byte)0xcb};
+      short num=0;
+
+      try {
+        InetAddress host = InetAddress.getByName(Host);
+        clientSocket = new Socket(host, Port);
+      }
+      catch(UnknownHostException ex) {
+        ex.printStackTrace();
+      }
+      catch(IOException e){
+        e.printStackTrace();
+      }
+      try {
+        DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream dOut = new DataOutputStream(clientSocket.getOutputStream());
+        dOut.write(Delivery_Conf);
+
+        dIn.read(rawinput);
+        num = (short)( (rawinput[3] & 0xFF) << 8 | (rawinput[4]&0xFF));
+        //System.out.println(String.format("%d %s %s",num,String.format("%02X", rawinput[3]),String.format("%02X", rawinput[4])));
+
+        dIn.close();
+        dOut.close();
+        clientSocket.close();
+      }
+      catch(IOException e){
+        e.printStackTrace();
+      }
+      Measurement measure = new Measurement ("level",Short.toString(num),num/(size/100));
       return measure;
   }
 
